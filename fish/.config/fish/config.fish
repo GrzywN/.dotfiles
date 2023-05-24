3a2baa1169f9c7e75e5c3853372f4396d8a650c1
Karol Binkowski <starystachu9@gmail.com>
2023-05-24 14:02:00 +0200

    feat: change zsh to fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+alias g git
+alias ls='ls --color=auto'
+alias ll='ls -la'
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH node_modules/.bin $PATH
+
+eval "$(starship init fish)"
