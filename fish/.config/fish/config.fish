e56407778e5886f37c3705f748324f64cc3a0856
Karol Binkowski <starystachu9@gmail.com>
2023-05-26 17:15:06 +0200

    feat: add pnpm to fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,3 +14,10 @@ set -gx PATH ~/.local/bin $PATH
 set -gx PATH node_modules/.bin $PATH
 
 eval "$(starship init fish)"
+
+# pnpm
+set -gx PNPM_HOME "/home/grzywn/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
