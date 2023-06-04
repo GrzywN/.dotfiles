7ef7e33fdd8faac2018502b6e0f1ed832efc5249
Karol Binkowski <starystachu9@gmail.com>
2023-06-04 15:15:16 +0200

    feat: make telescope show hidden files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,7 @@
 local nnoremap = require('grzywn.keymap').nnoremap
 local builtin = require('telescope.builtin')
 
-nnoremap('<leader>ff', builtin.find_files)
-nnoremap('<leader>fg', builtin.git_files)
+nnoremap('<leader>ff', '<cmd>Telescope find_files hidden=true<cr>')
 nnoremap('<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input('Grep > ' ) });
 end)
