7d4a43fc6633c773a3839d3d292c08097b128592
GrzywN <karolbinkowski3@proton.me>
2024-04-12 23:46:52 +0200

    feat: improve fish and i3 configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,26 @@
+# Fish config
 set fish_greeting ""
-
 set -gx TERM xterm-256color
 
-alias g git
+# Aliases
 alias ls='ls --color=auto'
 alias ll='ls -la'
 command -qv nvim && alias vim nvim
 command -qv nvim ~/todo.md && alias todo
 
+# Neovim
 set -gx EDITOR nvim
+
+# Binaries and scripts
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
 set -gx PATH ~/.local/bin $PATH
-set -gx PATH node_modules/.bin $PATH
+
+# PNPM
+set -gx PNPM_HOME "/home/grzywn/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
 
 # Flutter / Android SDK
 set -gx PATH /opt/flutter/bin $PATH
@@ -20,17 +28,8 @@ set -x PATH $PATH $HOME/.pub-cache/bin
 set -gx PATH /home/grzywn/Android/Sdk/platform-tools $PATH
 set -x JAVA_HOME /usr/lib/jvm/default
 set -x PATH $PATH $JAVA_HOME/bin
-set -x CHROME_EXECUTABLE firefox
-
-
-# pnpm
-set -gx PNPM_HOME "/home/grzywn/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
 set -x PATH "$PATH" "$HOME/flutter/bin"
+set -x CHROME_EXECUTABLE firefox
 
 # Fly.io
 set -x FLYCTL_INSTALL "/home/grzywn/.fly"
