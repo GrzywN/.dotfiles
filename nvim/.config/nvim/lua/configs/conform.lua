67d1fca96c580ed1333a525a4fd1620e961a85f7
GrzywN <karolbinkowski3@proton.me>
2024-04-14 00:21:05 +0200

    feat: add lsps, formatters and treesitters

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -5,10 +5,12 @@ local options = {
     html = { { "prettierd", "prettier" } },
     css = { { "prettierd", "prettier" } },
     javascript = { { "prettierd", "prettier" } },
+    go = { { "gofmt" }},
+    prisma = { { "prisma-fmt", "prisma format"} },
   },
 
   format_on_save = {
-    timeout_ms = 500,
+    timeout_ms = 150,
     lsp_fallback = true,
   },
 }
